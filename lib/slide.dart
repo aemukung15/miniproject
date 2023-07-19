8625fa8b75417231da0580604b529b178d59d517
aemukung15 <129814181+aemukung15@users.noreply.github.com>
2023-07-19 10:51:17 +0700

    อัปเดต หน้าคู่มือ

--- lib/slide.dart
+++ lib/slide.dart
@@ -1,42 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:miniproject/drawer.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-class Manual extends StatefulWidget {
-  const Manual({Key? key}) : super(key: key);
+class MySlide extends StatefulWidget {
+  const MySlide({Key? key}) : super(key: key);
 
   @override
-  State<Manual> createState() => _ManualState();
+  State<MySlide> createState() => _MySlideState();
 }
 
-class _ManualState extends State<Manual> {
-  int currentIndex = 6; // Declare currentIndex variable
+class _MySlideState extends State<MySlide> {
+  int currentIndex = 5; // Declare currentIndex variable
   CarouselController carouselController =
       CarouselController(); // Declare carouselController variable
 
   List<Map<String, dynamic>> imageList = [
-    {"id": 1, "image_path": 'assets/home_page.png'},
-    {"id": 2, "image_path": 'assets/hamburg.png'},
-    {"id": 3, "image_path": 'assets/record.png'},
-    {"id": 4, "image_path": 'assets/Tha_menu.png'},
-    {"id": 5, "image_path": 'assets/develop.png'},
-    {"id": 6, "image_path": 'assets/refer.png'},
+    {"id": 1, "image_path": 'assets/slide1.png'},
+    {"id": 2, "image_path": 'assets/slide2.png'},
+    {"id": 3, "image_path": 'assets/slide3.png'},
+    {"id": 4, "image_path": 'assets/slide4.png'},
+    {"id": 5, "image_path": 'assets/slide5.png'},
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'คู่มือ',
-          style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-      ),
-      endDrawer: const MyDrawer(),
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
@@ -50,7 +37,7 @@ class _ManualState extends State<Manual> {
           children: [
             // const SizedBox(height: 300),
             SizedBox(
-              height: 770,
+              height: 100,
               child: InkWell(
                 onTap: () {
                   showDialog(
@@ -99,35 +86,6 @@ class _ManualState extends State<Manual> {
                 ),
               ),
             ),
-            Positioned(
-              bottom: 10,
-              left: 0,
-              right: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: imageList.asMap().entries.map(
-                  (entry) {
-                    return GestureDetector(
-                      onTap: () => carouselController.animateToPage(entry.key),
-                      child: Container(
-                        width: currentIndex == entry.key ? 17 : 7,
-                        height: 7,
-                        margin: const EdgeInsets.symmetric(
-                          horizontal: 2.0,
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: currentIndex == entry.key
-                              ? const Color.fromARGB(255, 239, 214, 157)
-                              : const Color.fromARGB(255, 0, 0, 0),
-                        ),
-                      ),
-                    );
-                  },
-                ).toList(),
-              ),
-            ),
-            // Add more widgets here
           ],
         ),
       ),
